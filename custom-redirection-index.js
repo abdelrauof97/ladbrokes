a9d6d94553eff227e71ebe34ab78a26192b7347b
Abdelrauof Mo <34385173+abdelrauof97@users.noreply.github.com>
2018-12-01 19:20:38 +0200

    Added the old redirection

---
+++ custom-redirection-index.js
@@ -0,0 +1,19 @@
+
+<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+<script>
+  $.get("https://ipinfo.io", function(response) {
+      country = response.country;
+      region = response.region;
+     if(country=="AU"){
+       if (region == "Queensland" || region == "Northern Territory" || region == "Tasmania") {
+         $(".section").first().show();
+       } else {
+         $(".section:nth-child(3)").show();
+       }
+       $(".section").last().show();
+     } else {
+       $(".section:nth-child(4)").show();
+       $(".section").last().show();
+   }
+  }, "jsonp");
+</script>
